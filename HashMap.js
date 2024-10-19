1867e0a62c0a64ff8cd64287d0f14d0a70630a95
syeero7 <EmalHasitha@proton.me>
2024-10-19 06:49:31 +0530

    Add set method

--- HashMap.js
+++ HashMap.js
@@ -24,4 +24,24 @@ export default class HashMap {
     if (index < 0 || index >= this.#capacity)
       throw new Error("Trying to access index out of bound");
   }
+
+  set(key, value) {
+    const index = this.#hash(key);
+    const buckets = this.#buckets;
+
+    this.#tryAccess(index);
+    if (!buckets[index]) {
+      buckets[index] = [[key, value]];
+      return;
+    }
+    let valueUpdate = false;
+    for (const bucket in buckets[index]) {
+      if (buckets[index][bucket][0] === key) {
+        buckets[index][bucket][1] = value;
+        valueUpdate = true;
+        break;
+      }
+    }
+    if (!valueUpdate) buckets[index].push([key, value]);
+  }
 }
