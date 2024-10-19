28acc4e9d55a4b6404ae9a26d7ac6e66dd7eec2c
syeero7 <EmalHasitha@proton.me>
2024-10-19 07:31:31 +0530

    Test hash map

---
+++ main.js
@@ -0,0 +1,37 @@
+import HashMap from "./HashMap.js";
+
+const test = new HashMap(16, 0.75);
+// test.set("xd", "ko");
+// test.set("h", "dd");
+// test.set("half", "ku");
+test.set("apple", "red");
+test.set("banana", "yellow");
+test.set("carrot", "orange");
+test.set("dog", "brown");
+test.set("elephant", "gray");
+test.set("frog", "green");
+test.set("grape", "purple");
+test.set("hat", "black");
+test.set("ice cream", "white");
+test.set("jacket", "blue");
+test.set("kite", "pink");
+test.set("lion", "golden");
+test.set("moon", "silver");
+test.set("moon", "gold");
+test.set("apple", "green");
+test.set("some", "string");
+test.set("moon", "red");
+console.log(test.has("moon"));
+test.remove("moon");
+console.log(test.has("moon"));
+
+// console.log(test.has("go"));
+// console.log(test.has("xd"));
+// console.log(test.remove("xd"));
+// console.log(test.has("xd"));
+// console.log(test.get("h"));
+console.log(test.length());
+// console.log(test.keys());
+// console.log(test.values());
+console.log(test.entries());
+console.log(test.get("apple"));
